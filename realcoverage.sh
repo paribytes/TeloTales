1af170072edec31c8f957be1db70a391e22e3090
Priyanshi Shah <priyanshishah213@gmail.com>
2024-10-28 12:53:50 -0700

    Update realcoverage.sh

--- realcoverage.sh
+++ realcoverage.sh
@@ -13,7 +13,7 @@ do
     while read chr
     do
         grep "^$chr[[:space:]]" "$filename"
-        # Extract and tally telomere motif counts for each chromosome
+        # Extract and tally telomere read counts for each chromosome, [[:space:]] was used to avoid chr10,...chr19 being counted in chr1 and chr20,21,22 being counted in chr2
         x=$(grep "^$chr[[:space:]]" "$filename" | awk '{sum+=$2} END {print sum}')
 
         # Append results to stage2coverage file
@@ -21,7 +21,7 @@ do
     done < chrnames
 done < output_coverage_filenames
 
-# Remove the '_coverage.txt' part from the filenames in the final file
+# Remove the '_coverage.txt' part from the filenames in the final file and save the results to "stage2coverage" file
 sed -i -e 's/_stage2_coverage\.txt//g' stage2coverage
 
 
