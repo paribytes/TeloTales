00a25a18fa3777a4bd67b514b97c477cc34d9368
Priyanshi Shah <priyanshishah213@gmail.com>
2026-04-12 10:31:07 -0700

    Update download_files.sh
    Updated the NCBI link since the previous one was no longer active

--- scripts/download_files.sh
+++ scripts/download_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#This script (together with the "files_list.txt" file) will download WGS BAM files from Phase 3 of the 1000 Genomes Project by utilizing NCBI's site (https://www.ncbi.nlm.nih.gov/projects/faspftp/1000genomes/)
+#This script (together with the "files_list.txt" file) will download WGS BAM files from Phase 3 of the 1000 Genomes Project by utilizing NCBI's site (https://ftp-trace.ncbi.nih.gov/1000genomes/ftp/phase3/data/)
 
 #Since the base URL is the same for all files, I made the base URL a bash variable. All the file paths are stored in the "files_list.txt" file.
 
-base_url="https://ftp.ncbi.nlm.nih.gov/1000genomes/ftp/phase3/data"
+bash_url="https://ftp-trace.ncbi.nih.gov/1000genomes/ftp/phase3/data"
 
 #ensuring that the entire line is read as a whole so the link can work
 #file_path variable stores the content of each line from files_list.txt file
